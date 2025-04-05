d786f6f104cbd6c03dfa613366a57af883e26704
SADMAN-SADID-052 <sadman.cse8.bu@gmail.com>
2025-04-05 23:31:05 +0600

    design update and up;oad image

--- script.js
+++ script.js
@@ -1,30 +1,62 @@
 function openModal(albumId) {
-    const modal = document.getElementById("imageModal");
-    const modalContent = document.getElementById("modalContent");
+  const modal = document.getElementById("imageModal");
+  const modalContent = document.getElementById("modalContent");
 
-    // Clear previous images
-    modalContent.innerHTML = "";
+  modalContent.innerHTML = "";
 
-    // Define album images
-    const albumImages = {
-        packery: ["packery1.jpg", "packery2.jpg", "packery3.jpg"],
-        client: ["client1.jpg", "client2.jpg", "client3.jpg"],
-        beforeAfter: ["beforeAfter1.jpg", "beforeAfter2.jpg", "beforeAfter3.jpg"]
-    };
+  // Define album images
+  const albumImages = {
+    nature: [
+      "images/nature1.jpg",
+      "images/nature2.jpg",
+      "images/nature3.jpg",
+      "images/nature4.jpg",
+      "images/nature5.jpg",
+      "images/nature6.jpg",
+    ],
+    wildlife: [
+      "images/wildlife1.jpg",
+      "images/wildlife2.jpg",
+      "images/wildlife3.jpg",
+    ],
+    food: ["images/food1.jpg", "images/food2.jpg", "images/food3.jpg"],
+  };
 
-    // Add images to modal
-    if (albumImages[albumId]) {
-        albumImages[albumId].forEach(imgSrc => {
-            let img = document.createElement("img");
-            img.src = imgSrc;
-            img.classList.add("modal-img");
-            modalContent.appendChild(img);
-        });
-    }
+  // Add images with names to modal
+  if (albumImages[albumId]) {
+    albumImages[albumId].forEach((imgSrc) => {
+      let container = document.createElement("div");
+      container.classList.add("image-container");
 
-    modal.style.display = "flex";
+      let img = document.createElement("img");
+      img.src = imgSrc;
+      img.classList.add("modal-img");
+
+      
+      let imgName = imgSrc.split("/").pop().split(".")[0];
+
+      let caption = document.createElement("div");
+      caption.classList.add("image-caption");
+
+      container.appendChild(img);
+      container.appendChild(caption);
+      modalContent.appendChild(container);
+    });
+  }
+
+  modal.style.display = "flex";
 }
 
+// Close Modal Functions
 function closeModal() {
-    document.getElementById("imageModal").style.display = "block";
+  const modal = document.getElementById("imageModal");
+  modal.style.display = "none";
 }
+
+
+window.onclick = function (event) {
+  const modal = document.getElementById("imageModal");
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+};
